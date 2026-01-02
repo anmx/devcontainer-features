bf4d4b49a3ba443c94e2af067ba4bee2ae0de1e2
anmx <42076665+anmx@users.noreply.github.com>
2026-01-02 11:47:20 +0100

    1 devcontainer feature for just (#2)
    * Closes #1: Added initial just implementation

---
+++ src/just/install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+#
+# Description: Installs just, a command runner.
+
+set -e
+
+# Install dependencies
+(apt-get update && apt-get install -y --no-install-recommends curl ca-certificates) > /dev/null
+
+LATEST="$(curl -sL https://api.github.com/repos/casey/just/releases/latest | sed -n 's/.*"tag_name": "\([0-9.]*\)".*/\1/p')"
+VERSION="${VERSION:-latest}"
+
+# Validate and set VERSION
+if [[ "$VERSION" = "latest" ]]; then
+    VERSION="$LATEST"
+elif [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    # Valid semantic version, use as-is
+    :
+else
+    echo "Unsupported version: $VERSION"
+    exit 1
+fi
+
+INSTALLCOMPLETIONS="${INSTALLCOMPLETIONS:-false}"
+
+
+main() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Script must be run as root. Use sudo, su, or add \"USER root\" to your Dockerfile before running this script."
+        exit 1
+    fi
+
+    local OS
+    OS="$(uname -s)"
+
+    if [ "$OS" != "Linux" ]; then
+        echo "Unsupported OS: $OS"
+        exit 1
+    fi
+
+    local ARCH
+    ARCH="$(uname -m)"
+
+    local LIBC
+    LIBC="musl"
+
+    case "$ARCH" in
+    aarch64 | x86_64) ;;
+    armv7l)
+        LIBC="musleabihf"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+    esac
+
+    local TMP_DIR
+    TMP_DIR="$(mktemp -d)"
+
+    local URL
+    URL="https://github.com/casey/just/releases/download/$VERSION/just-$VERSION-$ARCH-unknown-linux-$LIBC.tar.gz"
+    echo "Downloading: $URL"
+
+    curl -L "$URL" | tar -xz -C "$TMP_DIR"
+
+    install -d /usr/local/bin
+    install -m 755 "$TMP_DIR/just" /usr/local/bin/
+
+    install -d /usr/local/share/man/man1
+    install "$TMP_DIR/just.1" /usr/local/share/man/man1/
+
+    if [ "$INSTALLCOMPLETIONS" = "true" ]; then
+
+        install -d /usr/share/bash-completion/completions
+        install "$TMP_DIR/completions/just.bash" /usr/share/bash-completion/completions/
+        install -d /usr/share/zsh/site-functions
+        install "$TMP_DIR/completions/just.zsh" /usr/share/zsh/site-functions/
+    fi
+
+    rm -rf "$TMP_DIR"
+    apt-get clean
+    rm -rf /var/lib/apt/lists/*
+
+    echo "Successfully installed just $VERSION"
+    just --version
+}
+
+main "$@"
