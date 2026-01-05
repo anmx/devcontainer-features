9a7c8f57e3ce2cf64b937f166e6727e80828da83
axel <a.c.neumann@gmail.com>
2026-01-05 14:51:45 +0100

    Validate Docusaurus server availability on port 3000 in test script

--- test/docusaurus/test.sh
+++ test/docusaurus/test.sh
@@ -5,7 +5,19 @@ set -e
 
 source dev-container-features-test-lib
 
-echo "Validating a Docusaurus version present..."
-check "Docusaurus has been installed" bash -c "npm --silent --prefix ./Docusaurus run docusaurus -- --version |  grep '[7-8]\.[0-9]*\.[0-9]*'"
+echo "Validating a Docusaurus serves on port 3000..."
+CMD=$(cat <<EOF
+curl --output /dev/null \
+    --retry-connrefused \
+    --retry-delay 1 \
+    --retry 3 \
+    --head \
+    --fail \
+    http://localhost:3000/
+EOF
+)
+check "serves at default port 3000" bash -c "${CMD}"
+
+#check "Docusaurus has been installed" bash -c "npm --silent --prefix ./Docusaurus run docusaurus -- --version |  grep '[7-8]\.[0-9]*\.[0-9]*'"
 
 reportResults
